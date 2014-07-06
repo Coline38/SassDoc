38fcbb61bd6f4a7754b9bf5c774356049d8470d3
Fabrice Weinberg <Fabrice@weinberg.me>
2014-07-06 19:51:52 +0200

    Test inline-search

--- examples/dist/assets/js/main.js
+++ examples/dist/assets/js/main.js
@@ -1,15 +1,11 @@
 /* global document, Fuse */
 'use strict';
 
-var search = (function () {
+var searchApi = (function(){
 
-  var getItems = function () {
-    return Array.prototype.slice.call(document.querySelectorAll('.sassdoc__item')).map(function (item) {
-      return { 
-        name: item.dataset.name, 
-        type: item.dataset.type, 
-        node: item
-      };
+  var getItems = function(){
+    return Array.prototype.slice.call(document.querySelectorAll('.sassdoc__item')).map(function(item){
+      return { name : item.dataset.name, type : item.dataset.type, node : item };
     });
   };
 
@@ -21,36 +17,56 @@ var search = (function () {
   var items = getItems();
   var index = new Fuse(items, options);
 
-  return index.search.bind(index);
+  var display = function(prop, item){
+    item.node.style.display = prop;
+  };
+
+  var displayItem = function(prop, items){
+    if ( Array.isArray(items) ){
+      items.forEach(display.bind(null, prop));
+    } else if (!!items.node) {
+      display(prop, items);
+    }
+  };
+
+  var hide = displayItem.bind(null, 'none');
+  var hideAll = hide.bind(null, items);
+
+  var show = displayItem.bind(null, 'block');
+  var showAll = show.bind(null, items);
+
+
+  var search = function(){
+    hideAll();
+    return index.search.apply(index, arguments);
+  };
+
+  return {
+    hide : hide,
+    hideAll : hideAll,
+
+    show : show,
+    showAll : showAll,
+
+    search : search
+  };
 })();
 
 
-(function (search) {
+(function(searchApi){
 
   var searchInput = document.querySelector('#js-search-input');
-  var searchSuggestions = document.querySelector('#js-search-suggestions');
 
-  var fillSuggestions = function (items) {
-    searchSuggestions.innerHTML = '';
-    items.slice(0, 10).forEach(function (item) {
-      var li = document.createElement('li');
-          li.innerHTML = '<a href="#' + item.type + '-' + item.name + '"><code>' + item.type.slice(0, 3) + '</code> ' + item.name + '</a>';
-      
-      searchSuggestions.appendChild(li);
-    });
-  };
-
-  var performSearch = function (term) {
-    var result = search(term);
-    fillSuggestions(result);
-  };
-
-
-  searchInput.addEventListener('keyup', function () {
-    performSearch(searchInput.value);
+  searchInput.addEventListener('keyup', function(){
+    if ( searchInput.value.length > 0 ){
+      searchApi.show(searchApi.search(searchInput.value));
+    } else {
+      searchApi.showAll();
+    }
   });
 
-  searchInput.addEventListener('search', function () {
-    performSearch(searchInput.value);
-  });
-})(search);
+})(searchApi);
+
+
+
+
